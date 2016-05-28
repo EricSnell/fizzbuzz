441eab662191502d804d4651d6ecd2b7fc4faeee
EricSnell <eric_snell21@yahoo.com>
2016-05-28 10:47:10 -0400

    Added Files

---
+++ js/fizzy.js
@@ -0,0 +1,23 @@
+var results = document.getElementById('displayResults');
+
+for (var i = 1; i < 101; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0){
+        
+        results.innerHTML = "FizzBuzz <br>";
+    
+    } else if (i % 3 == 0) {
+    
+        results.innerHTML = "Fizz <br>";
+    
+    } else if (i % 5 == 0) {
+        
+        results.innerHTML = "Buzz <br>";
+    
+    } else {
+        
+       results.innerHTML = i + "<br>";
+
+    }
+
+}
