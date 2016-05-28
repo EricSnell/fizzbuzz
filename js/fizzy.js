b3bd72e3054905086c5de4089a9a06a4508da430
EricSnell <eric_snell21@yahoo.com>
2016-05-28 12:16:20 -0400

    *working* FizzBuzz

--- js/fizzy.js
+++ js/fizzy.js
@@ -4,19 +4,19 @@ for (var i = 1; i < 101; i++) {
 
     if (i % 3 == 0 && i % 5 == 0){
         
-        results.innerHTML = "FizzBuzz <br>";
+        results.innerHTML += "FizzBuzz <br>";
     
     } else if (i % 3 == 0) {
     
-        results.innerHTML = "Fizz <br>";
+        results.innerHTML += "Fizz <br>";
     
     } else if (i % 5 == 0) {
         
-        results.innerHTML = "Buzz <br>";
+        results.innerHTML += "Buzz <br>";
     
     } else {
         
-       results.innerHTML = i + "<br>";
+       results.innerHTML += i + "<br>";
 
     }
 
